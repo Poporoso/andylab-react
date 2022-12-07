b74ba74049848b0d598a413e0ff13a441fdc0636
Andrea <c.birrillo@gmail.com>
2022-12-07 19:02:43 +0100

    Add step components

---
+++ src/components/booking/BookingStep.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import roomIcon from '../../assets/images/rooms.svg'
+import serviziIcon from '../../assets/images/servizi.svg'
+import infoIcon from '../../assets/images/info.svg'
+import anteprimaIcon from '../../assets/images/anteprima.svg'
+import promozioniIcon from '../../assets/images/promozioni.svg'
+
+import '../../assets/css/booking-step.css'
+
+const BookingStep = ({ step }) => {
+
+    return (
+        <div className="stepper-wrapper">
+            <button className={`stepper-item ${step >= 1 ? 'completed' : ''} ${step === 1 ? 'active' : ''}`}>
+                <div className="step-counter">
+                    <img src={roomIcon} alt="room" />
+                </div>
+                <div className="step-name">Rooms</div>
+            </button>
+            <button
+                className={`stepper-item ${step >= 2 ? 'completed' : ''} ${step === 2 ? 'active' : ''}`}>
+                <div className="step-counter">
+                    <img src={promozioniIcon} alt="Promozioni" />
+                </div>
+                <div className="step-name">Offerte</div>
+            </button>
+            <button className={`stepper-item ${step >= 3 ? 'completed' : ''} ${step === 3 ? 'active' : ''}`}>
+                <div className="step-counter">
+                    <img src={serviziIcon} alt="servizi" />
+                </div>
+                <div className="step-name">Servizi</div>
+            </button>
+            <button className={`stepper-item ${step >= 4 ? 'completed' : ''} ${step === 4 ? 'active' : ''}`}>
+                <div className="step-counter">
+                    <img src={infoIcon} alt="informazioni" />
+                </div>
+                <div className="step-name">Informazioni</div>
+            </button>
+            <div className={`stepper-item ${step >= 5 ? 'completed' : ''} ${step === 5 ? 'active' : ''}`}>
+                <div className="step-counter">
+                    <img src={anteprimaIcon} alt="Anteprima" />
+                </div>
+                <div className="step-name">Anteprima</div>
+            </div>
+        </div>
+    )
+}
+
+export default BookingStep
---
+++ src/components/booking/moduliRiepilogo/BookingNextStep.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from "react-router-dom";
+import { nextStep } from '../../../store/dataBookingSlice'
+
+const BookingNextStep = ({ nextStep: { pathPage, updateStep, lang, nexButtonStatus } }) => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch()
+
+    const stepList = {
+        'camere': {
+            step: 1,
+            apiUrl: `${lang}/booking/offerte-list/`,
+            pathUrl: `../offerte/`
+        },
+        'offerte': {
+            step: 2,
+            apiUrl: `${lang}/booking/service-list/`,
+            pathUrl: '../servizi/'
+        },
+        'servizi': {
+            step: 3,
+            apiUrl: `${lang}/booking/`,
+            pathUrl: '../informazioni/'
+        },
+        'informazioni': {
+            step: 4,
+            apiUrl: `${lang}/booking/`,
+            pathUrl: '../riepilogo/'
+        },
+        'riepilogo': {
+            step: 5,
+            apiUrl: `${lang}/booking/`,
+            pathUrl: '/'
+        }
+    }
+
+    const stepSelected = stepList[pathPage]
+
+    const handleNextStep = () => {
+        dispatch(
+            nextStep({
+                url: stepSelected.apiUrl,
+                dataJson: ''
+            })
+        )
+        navigate(`booking/${stepSelected.pathUrl}`)
+    }
+
+    useEffect(() => {
+        stepSelected?.step && updateStep(stepSelected.step)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    useEffect(() => {
+        updateStep(stepSelected.step)
+    }, [stepSelected.step, updateStep])
+
+    return (
+        stepSelected.step !== 5 &&
+        <button className='btn btn-primary' onClick={() => handleNextStep()} disabled={nexButtonStatus}>
+            Continua
+        </button>
+    )
+}
+
+export default BookingNextStep
